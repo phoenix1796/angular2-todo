0a13cc1ef26a177483a67f4d1882c8e2d828194e
Abhishek Chopra <phoenix.prog1796@gmail.com>
2016-12-22 11:23:54 +0530

    Converted to todo

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {todoComponent} from './todo.component';
+import {todoModel} from './todo.store';
+import {todoService} from './todo.service';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+        <todo *ngFor="let todo of todolist"
+          [item]="todo">
+          Loading...
+        </todo>
+      `,
+  providers:[todoService]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit {
+  constructor(private todoGetter:todoService) {}
+  ngOnInit() {
+    this.todoGetter.fetchTodos().subscribe((data)=>{
+      console.log(data);
+      this.todolist=data;
+    });
+  }
+  todolist: todoModel[] = [
+    {text: 'aasd'},
+    {text: 'qwer'}
+  ]
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {todoComponent} from './todo.component'
 import { AppComponent }  from './app.component';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule ,HttpModule],
+  declarations: [ AppComponent,todoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
